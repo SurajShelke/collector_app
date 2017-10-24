f0002a9e4c97e9e082a7eaed80a1bea361565b08
yogendra689 <yogendra@edcast.com>
2017-10-24 19:46:04 +0530

    trigger service

--- app/controllers/api/v1/webhook_controller.rb
+++ app/controllers/api/v1/webhook_controller.rb
@@ -2,11 +2,7 @@ class Api::V1::WebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def fetch_content
-    if params[:webhook_type] == "source_type"
-      SourceType.fetch_content(params[:id])
-    elsif params[:webhook_type] == "source"
-      Source.fetch_content(params[:id])
-    end
+    Collector::TriggerService.new(params).run
     head :ok
   end
 end
