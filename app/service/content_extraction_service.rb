f1d65f10f0cd05a7ee4c5da0b12a67ccb4596587
Darshan <darshan.patel1809@gmail.com>
2017-11-23 13:47:59 +0530

    EC-16 corrected file indentation

--- app/service/content_extraction_service.rb
+++ app/service/content_extraction_service.rb
@@ -4,8 +4,8 @@ module ContentExtractionService
   # file_path is either local file system path or you can also specify the URL of a document to be parsed.
   def get_file_content(file_path)
   	begin
-  		data = `#{java} -jar #{Rails.root.to_path}/vendor/tika-app-1.14.jar -t "#{file_path}"`
-  		data[0...AppConfig.max_file_content_size]
+  	  data = `#{java} -jar #{Rails.root.to_path}/vendor/tika-app-1.14.jar -t "#{file_path}"`
+  	  data[0...AppConfig.max_file_content_size]
   	rescue Exception => e
       Rails.logger.error "unable to get content for file name : #{name}\nfile_path : #{file_path}"
     end
