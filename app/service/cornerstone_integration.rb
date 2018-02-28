6b61dad8ed29b42b497b10b4c009f3aadc24f265
Darshan <darshan.patel1809@gmail.com>
2018-02-28 16:22:37 +0530

    [EC-125] added pagination and deep link url

--- app/service/cornerstone_integration.rb
+++ app/service/cornerstone_integration.rb
@@ -34,6 +34,10 @@ class CornerstoneIntegration < BaseIntegration
     '/services/api/Catalog/GlobalSearch'
   end
 
+  def lo_url
+    '/services/api/LO/GetDetails'
+  end
+
   def sign_signature(string_to_sign, key)
     key =  Base64.decode64(key)
     hmac = HMAC::SHA512.new(key)
@@ -45,19 +49,6 @@ class CornerstoneIntegration < BaseIntegration
     params.merge!('x-csod-date' => @current_date, 'Content-Type' => 'application/json', 'accept' => 'application/json')
   end
 
-  def fetch_catalogs
-    @current_date = Time.now.utc.strftime('%Y-%m-%dT%H:%M:%S.000')
-    string_to_sign = "GET\nx-csod-date:#{@current_date}\nx-csod-session-token:#{@access_token['data'].first['Token']}\n#{catalogs_url}"
-    signature = sign_signature(string_to_sign, @access_token['data'].first['Secret'])
-    headers = curl_headers('x-csod-session-token' => @access_token['data'].first['Token'], 'x-csod-signature' => signature)
-    url = "#{base_url}#{catalogs_url}"
-    @response = Curl.get(url) do |cur|
-      headers.each { |k, v| cur.headers[k] = v }
-    end
-    raise Webhook::Error::IntegrationFailure, "[CornerstoneIntegration] Failed to get catalogs, ErrorMessage: #{err.message}" unless response_data.key?('data')
-    response_data['data']
-  end
-
   def get_content(options = {})
     begin
       @host_name = @credentials['host_name']
@@ -65,8 +56,7 @@ class CornerstoneIntegration < BaseIntegration
       @api_key = @credentials['api_key']
       @secret_key = @credentials['secret_key']
       @access_token = access_token
-      catalogs = fetch_catalogs
-      catalogs.map { |entry| create_content_item(entry) }
+      fetch_catalogs
     rescue StandardError => err
       raise Webhook::Error::IntegrationFailure, "[CornerstoneIntegration] Failed Integration for source #{@credentials['source_id']} => Page: #{options[:page]}, ErrorMessage: #{err.message}"
     end
@@ -92,28 +82,73 @@ class CornerstoneIntegration < BaseIntegration
     end
   end
 
+  def fetch_catalogs
+    @current_date = Time.now.utc.strftime('%Y-%m-%dT%H:%M:%S.000')
+    string_to_sign = "GET\nx-csod-date:#{@current_date}\nx-csod-session-token:#{@access_token['data'].first['Token']}\n#{catalogs_url}"
+    signature = sign_signature(string_to_sign, @access_token['data'].first['Secret'])
+    headers = curl_headers('x-csod-session-token' => @access_token['data'].first['Token'], 'x-csod-signature' => signature)
+    page_number = 1
+    loop do
+      # url = "#{base_url}#{catalogs_url}"
+      url = "#{base_url}#{catalogs_url}?PageNumber=#{page_number}"
+      http = Curl::Easy.new(url) do |cur|
+        cur.headers = headers
+      end
+      http.perform
+      catalogs = JSON.parse(http.body_str)
+      raise Webhook::Error::IntegrationFailure, "[CornerstoneIntegration] Failed to get catalogs, ErrorMessage: #{err.message}" unless catalogs.key?('data')
+      break if catalogs['data'].count.zero?
+      catalogs['data'].map do |entry|
+        puts "#{entry['ObjectId']}----->#{entry['Title']}"
+        lo = fetch_lo_details(entry['ObjectId'])
+        puts lo
+        if lo['status'].to_i == 200 && lo['data'].count > 0
+          create_content_item(lo['data'][0]['trainingItem'])
+        else
+          Rails.logger.error "Lo not found, #{lo['message']}"
+        end
+      end
+      page_number += 1
+    end
+  end
+
+  def fetch_lo_details(lo_id)
+    begin
+      @current_date = Time.now.utc.strftime('%Y-%m-%dT%H:%M:%S.000')
+      string_to_sign = "GET\nx-csod-date:#{@current_date}\nx-csod-session-token:#{@access_token['data'].first['Token']}\n#{lo_url}"
+      signature = sign_signature(string_to_sign, @access_token['data'].first['Secret'])
+      headers = curl_headers('x-csod-session-token' => @access_token['data'].first['Token'], 'x-csod-signature' => signature)
+      url = "#{base_url}#{lo_url}?ObjectID=#{lo_id}"
+      http = Curl::Easy.new(url) do |cur|
+        cur.headers = headers
+      end
+      http.perform
+      JSON.parse(http.body_str)
+    rescue StandardError => err
+      raise Webhook::Error::IntegrationFailure, "[CornerstoneIntegration] Failed to get access token, ErrorMessage: #{err.message}"
+    end
+  end
+
   def content_item_attributes(entry)
     {
       external_id:     entry['ObjectId'],
       source_id:       @credentials['source_id'],
-      url:             '', # entry['pageUrl'],
+      url:             "#{base_url}#{URI.decode_www_form_component(entry['DeepLinkURL'])}",
       name:            sanitize_content(entry['Title']),
-      description:     sanitize_content(entry['Description']),
+      description:     sanitize_content(entry['Descr']),
       raw_record:      entry,
       content_type:    'course',
       organization_id: @credentials['organization_id'],
 
       additional_metadata: {
-        duration:      entry['Duration'],
-        price:         entry['Price'],
-        provider:      entry['Provider']
+        provider:      entry['Provider'],
+        Type:          entry['Type']
       },
 
       resource_metadata: {
         title:         sanitize_content(entry['Title']),
-        description:   sanitize_content(entry['Description']),
-        url:           '', # entry['pageUrl'],
-        images:        [{ url: entry['image'] }]
+        description:   sanitize_content(entry['Descr']),
+        url:           "#{base_url}#{URI.decode_www_form_component(entry['DeepLinkURL'])}"
       }
     }
   end
