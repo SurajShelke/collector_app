9d0bfa721029cdae56d1a2449f90499d995bedd6
Darshan Patel <darshan.patel1809@gmail.com>
2018-05-17 15:50:55 +0530

    indentation formatting:try2

--- app/service/file_parser.rb
+++ app/service/file_parser.rb
@@ -1,5 +1,5 @@
 class FileParser
-	attr_accessor :url, :root_element, :file_type
+  attr_accessor :url, :root_element, :file_type
 
 	#root_element is the node key in case of xml and for json it can be optional
   def initialize(url: , root_element:, file_type:, file_source:)
@@ -7,26 +7,26 @@ class FileParser
     @root_element = root_element
     @file_type = file_type
     @file_source = file_source
-	end
+  end
 
-	def parse_content
-		file_content, data = read_file, []
-		return [] if file_content.nil?
-		case @file_type
-		when 'json'
-			data = @root_element.present? ? JSON.parse(file_content)[@root_element] : JSON.parse(file_content)
-		when 'xml'
-			data = Hash.from_xml(file_content)[@root_element]
-		else
-			[]
-		end
-		data
+  def parse_content
+    file_content, data = read_file, []
+    return [] if file_content.nil?
+    case @file_type
+    when 'json'
+      data = @root_element.present? ? JSON.parse(file_content)[@root_element] : JSON.parse(file_content)
+    when 'xml'
+      data = Hash.from_xml(file_content)[@root_element]
+    else
+      []
+    end
+    data
   end
 
   def read_file
-  	case @file_source
-  	#[TODO] Testing purpose will remove in final commit
-  	when 'local'
+    case @file_source
+    #[TODO] Testing purpose will remove in final commit
+    when 'local'
   		File.read(@url)
   	when 'remote'
   		open(@url).read
